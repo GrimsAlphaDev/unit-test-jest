754ec0131df4a8e9d70583a31c17a8e013ffa821
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2024-10-02 14:27:21 +0700

    async await test

--- src/test/fetch.spec.ts
+++ src/test/fetch.spec.ts
@@ -1,9 +1,8 @@
 import { fetchPromise } from "../app/sum";
 
-test('the data is peanut butter', () => {
-    return expect(fetchPromise()).resolves.toBe('peanut butter');
+describe("fetch async test suite", () => {
+    test('the data is peanut butter', async() => {
+        const data = await fetchPromise();
+        expect(data).toBe('peanut butter');
+    })
 });
-
-test('the fetch fails with an error', () => {
-    return expect(fetchPromise()).rejects.toThrow('error');
-})
